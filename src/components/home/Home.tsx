e58068dc2875b4aa609af2975f63c4653dce8ef9
Deuos <patelku.2016@gmail.com>
2023-05-21 16:21:40 -0400

    Update

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className="container-md mx-auto bg-gray-200 border p-8 m-10">
-            <p>&lt;html mode="Dark"&gt;</p>
+        <div className="container-md relative mx-auto m-3 space-y-3">
+            <p className='text-white text-xs align-top text-opacity-20 ml-5'>&lt;html mode="Dark"&gt;</p>
+            <p className='text-white text-xs align-top text-opacity-20 ml-14'>&lt;body class="Welcome Page"&gt;</p>
         </div>
     )
 }
