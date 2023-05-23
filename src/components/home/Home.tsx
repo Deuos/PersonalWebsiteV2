1b8a8e044eafaa23159026b3af36cdfdeb5fa46d
Deuos <74274171+Deuos@users.noreply.github.com>
2023-05-22 21:33:34 -0400

    Update

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -28,27 +28,28 @@ const Home = () => {
             </div>
             <div className='container-md relative mx-auto m3 space-y-0 mt-20'>
                 <p className='text-white font-light text-xs align-top text-opacity-20 ml-40'>&lt;h1&gt;</p>
-
-                <div className='text-white text-center font-bold text-4xl my-10'>
-                    <TypeAnimation
-                        sequence={[
-                            'Hi, my name is Kush, and I am a broke',
-                            30,
-                            'Hi, my name is Kush, and I am a College Student',
-                            1000,
-                            'Hi, my name is Kush, and I am also a Web Developer',
-                            1000,
-                        ]}
-                        speed={15}
-                        repeat={0}
-                        cursor={false}
-                    />
+                <div className="border flex justify-center">
+                        <div className=" border font-bold text-4xl my-10 text-white">
+                            <TypeAnimation
+                                sequence={[
+                                    'Hi, my name is Kush, and I am a broke',
+                                    30,
+                                    'Hi, my name is Kush, and I am a College Student',
+                                    1000,
+                                    'Hi, my name is Kush, and I am also a Web Developer',
+                                    1000,
+                                ]}
+                                speed={15}
+                                repeat={0}
+                                cursor={false}
+                            />
+                        </div>
                 </div>
                 <p className='text-white font-light text-xs align-top text-opacity-20 ml-40'>&lt;/h1&gt;</p>
             </div>
-            <div className='container-md relative border mx-auto m3 space-y-0 mt-16'>
+            <div className='container-md relative mx-auto m3 space-y-0 mt-16'>
                 <p className='text-white font-light text-xs align-top text-opacity-20 ml-56'>&lt;p&gt;</p>
-                <div>
+                <div className='border mt-20 ml-94'>
                     <p>I like building Websites and Solving Problems.</p>
                     <p>View my Projects, Resume, Contact Me, or send me an email at Email@gmail.com.</p>
                 </div>
