e5d69c601945293f7dd75bdae5ca7c7859d035e3
Deuos <patelku.2016@gmail.com>
2023-06-07 12:16:41 -0400

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router basename="/PersonalWebsiteV2">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
